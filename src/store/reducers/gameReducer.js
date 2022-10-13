80dca86a4055f27bcd9f49b166704adb07cbf03b
victorcngo <hello@victorcango.fr>
2022-10-13 11:23:06 +0200

    feat(reducers): create gameReducer

---
+++ src/store/reducers/gameReducer.js
@@ -0,0 +1,29 @@
+import { ROLL_DICES } from "../constants/actions";
+
+/* Source of truth */
+const gameState = {
+  firstDiceResult: null,
+  secondDiceResult: null,
+  thirdDiceResult: null,
+  total: 0,
+};
+
+const reducer = (state = gameState, action = {}) => {
+  switch (action.type) {
+    case ROLL_DICES:
+
+      return {
+        ...state,
+        // attribuer une valeur random à firstDice
+        // attribuer une valeur random à secondDice
+        // attribuer une valeur random à thirdDice
+        
+        // set total = la somme des résultats
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
